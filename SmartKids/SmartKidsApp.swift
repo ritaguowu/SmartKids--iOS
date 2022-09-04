7810043241f9a3f2713eeabc9b31795c05e9551b
ritaguowu <ritaguowu@gmail.com>
2022-09-04 01:18:06 -0400

    Initial Commit

---
+++ SmartKids/SmartKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartKidsApp.swift
+//  SmartKids
+//
+//  Created by Wu Guo on 2022-09-04.
+//
+
+import SwiftUI
+
+@main
+struct SmartKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
